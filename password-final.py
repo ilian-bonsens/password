13d07ffc400d1ce93835ef8c5b0bdaa8db8ddb85
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-12-07 14:12:02 +0100

    version finale commentee

--- password-final.py
+++ password-final.py
@@ -8,41 +8,42 @@ token = Fernet(key)
 print(f"\nClé de chiffrement : {key.decode()}\n")
 
 
-def store_password(password):
+def store_password(password): # Fonction qui stocke le mot de passe dans un fichier json
     while True:
+# Vérifie si le mot de passe respecte les conditions 8 caractères, 1 minuscule, 1 majuscule, 1 chiffre, 1 caractère spécial
         if not (len(password) >= 8 and any(c.islower() for c in password) and any(c.isupper() for c in password) 
                 and any(c.isdigit() for c in password) and any(c in "*%@$!#^" for c in password)):
             print("\n[-] Le mot de passe ne respecte pas les conditions.\n")
             return
-        
+        # Hash le mot de passe
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
         password_data = {'hash': hashed_password}
         file_name = 'passwords.json'
-
+# Vérifie si le fichier json existe : si oui on le charge, si non on crée une liste vide qui va accueillir
         if os.path.exists(file_name):
             with open(file_name, 'r') as file:
                 data = json.load(file)
-
+# Vérifie si le mot de passe existe déjà dans le fichier json grace au hash
             if any(d['hash'] == hashed_password for d in data):
                 print("\n[+] Ce mot de passe existe déjà.\n")
                 return
-            
+# Ajoute le hash du mot de passe dans le fichier json          
             data.append(password_data)
             with open(file_name, 'w') as file:
                 json.dump(data, file, indent=1)
                 break
-        else:
+        else: # Sinon, crée le fichier json et ajoute le hash du mot de passe
             with open(file_name, 'x') as file:
                 json.dump([password_data], file, indent=1)
                 break
 
-def voir_passwords():
+def voir_passwords(): # Fonction qui affiche les hashes stockés dans le fichier json
     while True:
         securite = input("\nEntrez la clé de chiffrement : ")
-
+# Vérifie si la clé de chiffrement est correcte
         if securite == key.decode():
             file_name = 'passwords.json'
-
+# Vérifie si le fichier json existe : si oui on le charge, si non on crée une liste vide
             if os.path.exists(file_name):
                 with open(file_name, 'r') as file:
                     data = json.load(file)
@@ -56,14 +57,16 @@ def voir_passwords():
 
 def generer_password():
     while True:
-        length = random.randint(8, 24)
+    # Génère une longueur de mot de passe aléatoire entre 8 et 24 caractères
+        length = random.randint(8, 24) 
+    # Génère un mot de passe aléatoire de la longueur définie avec des lettres, chiffres, caractères spéciaux
         password = ''.join(random.choice(string.ascii_letters + string.digits + "*%@$!#^") for _ in range(length))
-
+    # Vérifie si le mot de passe respecte les conditions
         if (len(password) >= 8 and any(c.islower() for c in password) and any(c.isupper() for c in password) 
             and any(c.isdigit() for c in password) and any(c in "*%@$!#^" for c in password)):
             return password
 
-
+            # Menu
 while True:
     print("1. Créer un mot de passe")
     print("2. Générer un mot de passe")
